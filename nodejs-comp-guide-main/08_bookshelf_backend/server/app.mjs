6b54999bcc515139c9e9fb23d7e700770baf2f4b
hayashibara ibuki <ibuki.hayashibara@gmail.com>
2025-06-14 15:56:19 +0900

    a

--- nodejs-comp-guide-main/08_bookshelf_backend/server/app.mjs
+++ nodejs-comp-guide-main/08_bookshelf_backend/server/app.mjs
@@ -1,29 +1,12 @@
 import express from "express";
-import env from "dotenv";
-env.config();
-
-import apiRoutes from "./api-routes/index.mjs";
-import "./helpers/db.mjs";
-
+import apiRoutes from "."
 const app = express();
 const port = process.env.PORT || 8080;
-
 app.use(express.json());
 
-// API
-app.use('/api', apiRoutes);
-
-app.use(function(req, res) {
-    res.status(404).json({ msg: "Page Not Found" });
-});
-
-app.use(function(err, req, res, next) {
-    if(res.headersSent) {
-        return next(err);
-    }
-    res.status(500).json({ msg: '不正なエラーが発生しました。'});
-});
+//API
+app.use(apiRoutes);
 
 app.listen(port, () => {
-    console.log(`Server Start: http://localhost:${port}`);
+    console.log(`server start: http://localhost:${port}`);
 });
